88ea724b27381eb725aae9031f973b65b444f0b0
MatthewPena2 <pena.matthew05@gmail.com>
2025-03-09 21:18:20 -0700

    Completed the SimulationEventQueue implementation.

--- src/Main/Java/Queue/SimulationEventQueue.java
+++ src/Main/Java/Queue/SimulationEventQueue.java
@@ -1,133 +1,99 @@
 package Queue;
 
-//> Created for the solution
-//Final solution for Lab 6, should be in queue package 
+//import QueuePackage interfaces and resources
 import QueuePackage.*;
-//to
-//package QueuePackage; 
-
 import java.util.*;
 
-/**
- * A class that will implement the event queue.  
- * 
- * 
- * @author Charles Hoot 
- * @version 5.0
- */
+//start of class
+public class SimulationEventQueue implements SimulationEventQueueInterface {
 
-    
-public class SimulationEventQueue implements SimulationEventQueueInterface
-{
-    private Vector<SimulationEvent> queue;// queue front is first in the vector
-    private double theCurrentTime;
+    private Vector <SimulationEvent> queue;
+    private double simulationTime;
 
-    /**
-     * Constructor for objects of class BankLine
-     */
-    public SimulationEventQueue()
-    {
-        queue = new Vector<SimulationEvent>();
-        theCurrentTime = 0.0;
-    } // end constructor
-    
-    public void add(SimulationEvent newEvent)
-    {
-        if(newEvent.getTime() < theCurrentTime)
-        {
-            //System.out.println("Fails to add event... before the current time");
+    //constructor - will be used later in BankLine
+    public SimulationEventQueue() {
+        queue = new Vector <SimulationEvent>();
+        simulationTime = 0.0;
+    }
+
+    //add method - save for later
+    public void add(SimulationEvent newEntry) {
+        if(newEntry.getTime() < simulationTime){ //if the time is less than the simulation time, throw exception
+            //Since a test is designed to fail, use a print statement as an exception would prevent further testing
+            System.out.println("Cannot add an event with a time less than the current time");
+
+            //exception statement would appear as follows:
+            //throw new EmptyQueueException("Cannot add an event with a time less than the current time");
+
+        }else if(isEmpty()){// if the queue is empty, add new entry
+            queue.add(newEntry); //using built-in vector add method, add new entry into the event queue
+
+        }else{
+            //look through the queue to find an appropriate position to add the entry
+            //can iterate through the queue using the Java iterator object
+            //saves from having to write our own code that iterates through the queue
+            Iterator<SimulationEvent> iterate = queue.iterator();
+            boolean largerEntry = false;
+            int index = -1;
+            //since an index is an items position - 1, we can initialize the index at -1
+            //otherwise, we would subtract one from the position to get the index
+            while(iterate.hasNext() && !largerEntry ){
+                //while the queue still has entries and a larger time value has not been found
+                index++; //increment index
+                SimulationEvent nextEntry = iterate.next(); //grab next entry
+                if(nextEntry.getTime() > newEntry.getTime()){ //compare new entry with next entry
+                    largerEntry = true; //if the next entry's time is larger, set largerEntry to true
+                }
+            }
+
+            if(largerEntry){ //if a larger entry has been found
+                queue.add(index, newEntry); //add at index before larger entry
+            }else{
+                queue.add(newEntry); //otherwise, add it at the back of the queue
+            }
         }
-        else if(queue.isEmpty())
-        {
-            // Just add it in
-            queue.add(newEvent);
-            //System.out.println("   added to empty queue");
+    }
+
+    //peek method
+    public SimulationEvent peek() {
+        SimulationEvent first = null;
+        if(isEmpty()) {
+            throw new EmptyQueueException ("The queue is empty. There is no event to pull.");
+            //since null values can be valid entries, throw an exception instead of returning null
+        }else{
+            first = queue.get(0); //using the built-in vector .get method, return the first element
         }
-        else
-        {
-            // Iterate over the queue to find the position to add the item.
-            System.out.println("Adding an event " + newEvent + " at time " + newEvent.getTime());
-            int pos = -1;
-            boolean foundLarger = false;
-            Iterator<SimulationEvent> iter = queue.iterator();
-            while(iter.hasNext() && !foundLarger)
-            {
-                pos++;
-                SimulationEvent check = iter.next();
-                //System.out.println("  checked against time " + check.getTime() );
-                
-                // Events at the same time should go in the order that they were
-                // created, so we want to find the position of the first event with
-                // a time greater than our time.
-                foundLarger = check.getTime() > newEvent.getTime();
-            }
-            
-            System.out.println("   pos is " + (pos));
-            System.out.println("   foundLarger is " + (foundLarger));
-            
-            if(!foundLarger)
-            {
-                queue.add(newEvent);        // add to end
-                System.out.println("   added to end ");
-            }
-            else 
-            {
-                queue.add(pos,newEvent);        // legal to add
-                System.out.println("   added to position " + (pos));
-            }
-         }       
-    } // end add
+        return first;
+    }
 
-    public SimulationEvent peek()
-    {
-        SimulationEvent front = null;
-        if (isEmpty())
-            throw new EmptyQueueException("Attempting to access entries on an empty queue.");
-        else
-            front = queue.get(0);
-        return front;
-    } // end get
-    
+    //remove method
+    public SimulationEvent remove(){
+        SimulationEvent item = null;
+        if(isEmpty()) { //if the queue is empty, throw out empty queue exception
+            throw new EmptyQueueException ("The queue is empty. There is no event to remove");
+        }else{ //if the queue is not empty...
+            item = queue.get(0); //get the first event in the queue and store it in the item variable
+            queue.remove(0); //remove the top event
+            simulationTime = item.getTime(); //set the simulation time as the time from last removed
+        }
+        return item;
+    }
 
-    public SimulationEvent remove() {
-        SimulationEvent front = null;
-        if (isEmpty()) {
-            throw new EmptyQueueException("Attempting to access entries on an empty queue.");
-        } else {
-            front = queue.get(0);
-            queue.remove(0);
-            theCurrentTime = front.getTime();
-            //System.out.println("Removed the first event " + front + " time is now " + theCurrentTime);
-        } // end if
-        return front;
-    } // end remove
+    //we can get the return values for each method using the built-in Java Vector methods
+    public boolean isEmpty() {
+        return queue.isEmpty(); //check if a queue is empty
+    }
 
-    public boolean isEmpty()
-    {
-        return queue.isEmpty();
-    } // end isEmpty
-    
-    public void clear()
-    {
-        queue.clear();
-    } // end clear
+    public int getSize(){
+        return queue.size(); //return the size of the queue
+    }
 
-	/** Gets the size of the event queue.
-        * @return The number of entries currently in the event queue.
-        */
-	public int getSize()
-	{
-	   return queue.size();
-	}
-	
-	/**
-	 * Get the current time of the simulation
-	 * 
-	 * @return The time for the first event on the queue.
-	 */
-	public double getCurrentTime()
-	{
-	   return theCurrentTime;
-	}
+    public void clear(){
+        queue.clear(); //return nothing; will only clear the queue
+    }
+
+    public double getCurrentTime(){
+        return simulationTime; //return the current time of the event
+    }
 
 }
